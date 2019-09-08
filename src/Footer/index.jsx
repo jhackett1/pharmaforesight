6b39a9a881f86e70876f2cf93d410acd5fecb9a1
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-09-08 14:46:27 +0100

    temporary a11y fixes

--- src/Footer/index.jsx
+++ src/Footer/index.jsx
@@ -3,8 +3,8 @@ import React from "react"
 const Footer = () =>
     <footer>
         <p>© PharmaForesight 2019</p>
-        <a href="#">Privacy</a>
-        <a href="#">Contact</a>
+        <a href="/">Privacy</a>
+        <a href="/">Contact</a>
     </footer>
 
 export default Footer
--- src/Header/index.jsx
+++ src/Header/index.jsx
@@ -5,8 +5,8 @@ const Header = () =>
         <h1>PharmaForesight</h1>
         <p>To get started, answer some questions about your trial.</p>
         <nav>
-            <a href="#">How it works</a>
-            <a href="#">Contact</a>
+            <a href="/">How it works</a>
+            <a href="/">Contact</a>
         </nav>
     </header>
 
