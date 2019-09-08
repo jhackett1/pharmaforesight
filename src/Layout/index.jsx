4ab4c2eba2ec66671fec1f261fccea0f60cda162
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-09-08 14:45:44 +0100

    add google web font

--- src/Layout/index.jsx
+++ src/Layout/index.jsx
@@ -1,11 +1,22 @@
 import React from "react"
 import Header from "../Header"
 import Footer from "../Footer"
+import { createGlobalStyle } from "styled-components"
+
+const Style = createGlobalStyle`
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: 'Karla', sans-serif;
+    }
+`
 
 const Layout = ({
     children
 }) =>
     <>
+        <Style/>
         <Header/>
         {children}
         <Footer/>
