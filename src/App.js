d0f6a0da70ccb5b7bf3cd44d82d1db9df2021115
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-09-12 22:29:28 +0100

    start building dial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from "react"
 import Layout from "./Layout"
+import styled from "styled-components"
+import Dial from "./Dial"
+
+const Columns = styled.div`
+  max-width: 1100px;
+  margin: 0 auto;
+  padding: 25px;
+  @media screen and (min-width: 850px) {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-column-gap: 60px;
+  }
+`
 
 const App = () =>
   <Layout>
-    <h1>App</h1>
+    <Columns>
+      <h1>App</h1>
+      <Dial/>
+    </Columns>
   </Layout>
 
 export default App
--- src/Dial/index.jsx
+++ src/Dial/index.jsx
@@ -1,8 +1,56 @@
 import React from "react"
+import styled from "styled-components"
+import theme from "../_theme"
 
-const Dial = () =>
-    <div>
+const Container = styled.div`
+    width: 100%;
+    height: 0;
+    padding-bottom: 100%;
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
 
-    </div>
+const Outer = styled.div`
+    width: 100%;
+    height: 100%;
+    border-radius: 100%;
+    flex: 1;
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    padding: 20px;
+    background: linear-gradient(${theme.brightPurple}, ${theme.brightBlue})
+`
+
+const Inner = styled.div`
+    width: 100%;
+    height: 100%;
+    border-radius: 100%;
+    background: ${theme.pageBackground};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+const Message = styled.p`
+    color: ${theme.lighterPlaceholder};
+    font-size: 1.2em;
+    text-align: center;
+    max-width: 200px;
+`
+
+const Dial = ({
+    condition,
+    score 
+}) =>
+    <Container>
+        <Outer>
+            <Inner>
+                <Message>Answer some questions to get started</Message>
+            </Inner>
+        </Outer>
+    </Container>
 
 export default Dial
