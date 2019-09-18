30fa65e02643fb599b802538505f23a066ab1d39
Jaye <joshua.hackett93@gmail.com>
2019-09-18 11:24:57 +0100

    added simple, basic, password auth

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react"
 import ReactDOM from "react-dom"
 import App from "./components/App"
 import DataContextProvider from "./contexts/DataContext"
+import PasswordGate from "./components/PasswordGate"
 
 ReactDOM.render(
     <DataContextProvider>
-        <App/>
+        <PasswordGate>
+            <App/>
+        </PasswordGate>
     </DataContextProvider>, 
     document.getElementById("root")
 )
