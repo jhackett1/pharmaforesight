1b0ebcabe1cee1b4cb101b6cf8d3fd027bd45266
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-09-23 16:06:08 +0100

    date fixed

--- src/lib/actions.js
+++ src/lib/actions.js
@@ -1,10 +1,13 @@
 import { getDetails, getProbability } from "./api"
 
 export const makePrediction = async (formData) => {
+
+    console.log(formData.start_date)
+
     let processedData = {
         ...formData,
-        start_date: formData.start_date ? `${formData.start_date.getFullYear()}-${formData.start_date.getMonth()}-${formData.start_date.getDate()}` : "",
-        end_date: formData.end_date ? `${formData.end_date.getFullYear()}-${formData.end_date.getMonth()}-${formData.end_date.getDate()}` : "",
+        start_date: formData.start_date ? `${formData.start_date.getFullYear()}-${formData.start_date.getMonth()+1}-${formData.start_date.getDate()}` : "",
+        end_date: formData.end_date ? `${formData.end_date.getFullYear()}-${formData.end_date.getMonth()+1}-${formData.end_date.getDate()}` : "",
     }
     const detailedData = await getDetails(processedData)
     const probability = await getProbability(detailedData)
