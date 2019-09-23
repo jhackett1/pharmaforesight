e832624631bebcc100ff5433bf83b0ca7034525a
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-09-23 14:54:16 +0100

    scaffold a refine function

--- src/lib/actions.js
+++ src/lib/actions.js
@@ -1,19 +1,20 @@
 import { getDetails, getProbability } from "./api"
 
 export const makePrediction = async (formData) => {
-
     let processedData = {
         ...formData,
         start_date: formData.start_date ? `${formData.start_date.getFullYear()}-${formData.start_date.getMonth()}-${formData.start_date.getDate()}` : "",
         end_date: formData.end_date ? `${formData.end_date.getFullYear()}-${formData.end_date.getMonth()}-${formData.end_date.getDate()}` : "",
     }
-
-    console.log(processedData)
-
     const detailedData = await getDetails(processedData)
     const probability = await getProbability(detailedData)
     return {
         probability: probability.pos,
         detailedData
     }
+}
+
+
+export const refinePrediction = async () => {
+
 }
